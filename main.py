49ef797b171b9f6941bbda97496b4a0a25e6e51e
Estefania Delgado <lunapirroagustin@gmail.com>
2024-07-25 22:45:45 -0300

    Nueva ruta de titi

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def pela():
     return "Hola que haces"
 
+@app.route("/Titi")
+def titi():
+    return "Su amigo el negro"
+
 
 if __name__ == 'main':
     app.run(debug=True)
