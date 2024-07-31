d840347935d51b9c35ee2caf254c9cdbe4eb7d51
ValenOsuna <OsunaValentino@gmail.com>
2024-07-30 22:57:58 -0300

    Agregado de Core y Class : Vuelo

---
+++ Core/Vuelo/VueloClass.py
@@ -0,0 +1,8 @@
+class Vuelo():
+    def __init__(self, Nro , Destino , Origen , HorarioDespegue , HorarioEmbarque):
+        self.Nro = Nro
+        self.Destino = Destino
+        self.Origen = Origen
+        self.HorarioDespegue = HorarioDespegue
+        self.HorarioEmbarque = HorarioEmbarque
+
