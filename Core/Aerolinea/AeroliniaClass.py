d0f385afb10ca5f0e12296566ea19f675b616980
ValenOsuna <OsunaValentino@gmail.com>
2024-08-01 21:49:21 -0300

    Correccion de datos de las tablas

--- Core/Aerolinea/AeroliniaClass.py
+++ Core/Aerolinea/AeroliniaClass.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import relationship
 class Aerolinea():
     __tablename__= "Aerolineas"
 
+    id = Column("id", Integer , autoincrement= True , unique= True , primary_key= True)
     nombre = Column("nombre", String, primary_key= True)
     sigla = Column("sigla", String)
-    lista_vuelos = Column("lista_vuelos", Integer , ForeignKey("Vuelo.ID"))
-    VueloDetalle = relationship("Vuelo", back_populates="Aerolinea", cascade="all, delete")
+    lista_vuelos = Column("lista_vuelos", Integer , ForeignKey("Vuelo.id"))
+    VueloDetalle = relationship("vuelo", back_populates="Aerolinea", cascade="all, delete")
     
     def __init__(self,
                  nombre="",
