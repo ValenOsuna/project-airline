37db6f5ef9a3a9b9afb2c6df39ef398cc870db55
unknown <joshetito29@gmail.com>
2024-08-28 16:20:37 -0300

    actuality of the route ticket

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from flask import Flask, request
 from flask import jsonify
-from db import Base , ENGINE
+from db import Base, ENGINE
 
-from business import airline , flight, ticket
+from business import airline, flight, ticket
 from business import Flights, Airlines, Ticket
-
 
 
 app = Flask(__name__)
 
-app.register_blueprint(airline,url_prefix="/airline")
-app.register_blueprint(flight,url_prefix="/flight")
-app.register_blueprint(ticket,url_prefix="/ticket")
+app.register_blueprint(airline, url_prefix="/airline")
+app.register_blueprint(flight, url_prefix="/flight")
+app.register_blueprint(ticket, url_prefix="/ticket")
 
 if __name__ == "__main__":
     app.run(debug=True)
