b7559a0cda5fdf7945afea65d374c74e13326665
unknown <lunapirroagustin@gmail.com>
2024-08-29 23:12:57 -0300

    agregado de clases y controllers

---
+++ business/luggage/models/luggageClass.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, String, Integer , ForeignKey
+from sqlalchemy.orm import relationship
+from db import Base , Session
+
+class Luggages(Base):
+    __tablename__= "luggages"
+
+    id = Column("id", Integer , autoincrement= True , unique= True , primary_key= True)
+    type = Column("type", Integer, nullable =False)
+    weight = Column("weigth", Integer, nullable=False)
+    type_flight = Column("type_flight", Integer, nullable=False)
+
+    def __init__(self, type="", weight="", type_flight=""):
+        self.type=type
+        self.weight=weight
+        self.type_flight=type_flight
+
+    def create(self, Data):
+        self.type = Data["type"]
+        self.weight = Data["weight"]
+        self.type_flight = Data["type_flight"]
+        session = Session()
+        session.add(self)
+        session.commit()
+        session.close()
---
+++ business/pasenger/models/pasenger.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from db import Base , Session
+
+
+class Pasengers(Base):
+    __tablename__ = "pasengers"
+
+    id = Column("id", Integer , autoincrement= True , unique= True , primary_key= True)  
+    number_pasaport = Column("number_pasaport", Integer , nullable = False)
+    day_pasaport = Column("day_pasaport", Integer , nullable = False)
+    nationality = Column("acronym", String, nullable=False)
+    country_emision = Column("country_emision". String, nullable = False)
+
+    def __init__(self, number_pasaport="", day_pasaport="", nationality="", country_emision=""):
+        self.number_pasaport=number_pasaport
+        self.day_pasaport=day_pasaport
+        self.nationality=nationality
+        self.country_emision=country_emision
+
+    def create(self, Data):
+        self.number_pasaport = Data["number_pasaport"]
+        self.day_pasaport = Data["day_pasaport"]
+        self.nationality = Data["nationality"]
+        self.country_emision = ["country_emision"]
+        session = Session()
+        session.add(self)
+        session.commit()
+        session.close()
+
