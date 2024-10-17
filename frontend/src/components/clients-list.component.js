539f5c9ce8afe3a9586af8d7a731eacd10e6dd2e
unknown <joshetito29@gmail.com>
2024-10-17 15:05:34 -0300

    nose que hice mal

--- frontend/src/components/clients-list.component.js
+++ frontend/src/components/clients-list.component.js
@@ -6,29 +6,49 @@ export default class ClientList extends Component{
 
     constructor(props){
         super(props);
+        this.getClient = this.getClient.bind(this);
+        this.onChangeId = this.onChangeId.bind(this);
         this.state = {
-            getClient: null
+            id: null,
+            data: null
         };
     }
-    
     getClient(){
-        ClientDataServices.get()
+        var id_client = this.state.id
+        //var content = " "
+        //const [content, setcontent ] = useState(' ');
+        ClientDataServices.get(id_client)
             .then(response => {
-                console.log(response.data);
+                console.log (response.data);
+                //setcontent(response.data);
+                //document.getElementById('data').innerText = JSON.stringify(response.data);
+                document.getElementById('data').innerText = response.data.nationality;
             })
             .catch(e => {
                 console.log(e);
             });
     }
+    onChangeId(e){
+        this.setState({
+            id: e.target.value
+        });
+    }
 
     render(){
-        const { getClient } = this.state;
-        return (<button
-            className= "btn"
-            type = "button"
-            onClick= {this.getClient}>
-                buscar
-        </button>);
+        return (
+            <div className="container mt-3">
+                <form>
+                    <div className="mb-3">
+                    <label className="form-label">Id del Cliente</label>
+                    <input type="text" className="form-control" id="id" value={ this.state.id} onChange={this.onChangeId} />
+                    <div id="emailHelp" className="form-text"></div>
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={ this.getClient }>Buscar</button>
+                </form>
+                <div className="mb-3 col-md-4" id="data">
+                    datos:
+                </div>
+            </div>);
     }
 
     
