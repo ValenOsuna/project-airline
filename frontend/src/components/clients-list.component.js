b087e3277f4abfecde2161aa1f85e21993099c0a
Jonathan Aguiar <jonathan.aguiar@uala.com.ar>
2024-10-17 22:27:07 -0300

    add changes frontend

--- frontend/src/components/clients-list.component.js
+++ frontend/src/components/clients-list.component.js
@@ -6,49 +6,29 @@ export default class ClientList extends Component{
 
     constructor(props){
         super(props);
-        this.getClient = this.getClient.bind(this);
-        this.onChangeId = this.onChangeId.bind(this);
         this.state = {
-            id: null,
-            data: null
+            getClient: null
         };
     }
+    
     getClient(){
-        var id_client = this.state.id
-        //var content = " "
-        //const [content, setcontent ] = useState(' ');
-        ClientDataServices.get(id_client)
+        ClientDataServices.get()
             .then(response => {
-                console.log (response.data);
-                //setcontent(response.data);
-                //document.getElementById('data').innerText = JSON.stringify(response.data);
-                document.getElementById('data').innerText = response.data.nationality;
+                console.log(response.data);
             })
             .catch(e => {
                 console.log(e);
             });
     }
-    onChangeId(e){
-        this.setState({
-            id: e.target.value
-        });
-    }
 
     render(){
-        return (
-            <div className="container mt-3">
-                <form>
-                    <div className="mb-3">
-                    <label className="form-label">Id del Cliente</label>
-                    <input type="text" className="form-control" id="id" value={ this.state.id} onChange={this.onChangeId} />
-                    <div id="emailHelp" className="form-text"></div>
-                    </div>
-                    <button type="submit" className="btn btn-primary" onClick={ this.getClient }>Buscar</button>
-                </form>
-                <div className="mb-3 col-md-4" id="data">
-                    datos:
-                </div>
-            </div>);
+        const { getClient } = this.state;
+        return (<button
+            className= "btn"
+            type = "button"
+            onClick= {this.getClient}>
+                buscar
+        </button>);
     }
 
     
