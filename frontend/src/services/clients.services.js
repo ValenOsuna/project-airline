9f815e83337b845e95b4ffb6e2883c8da34da9d4
Agustin <lunapirroagustin@gmail.com>
2024-10-28 16:26:36 -0300

    tarea

--- frontend/src/services/clients.services.js
+++ frontend/src/services/clients.services.js
@@ -5,6 +5,10 @@ class ClientsDataService{
     get(id){
         return client_api.post(`/passenger/search`,{id:id})
     }
+    create(id){
+        return client_api.post('/passenger/create',{id:id})
+    }
 }
 
 export default new ClientsDataService();
+
