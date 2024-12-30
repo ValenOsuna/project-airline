dda3e2c89426f41ce954929279497a844e22aa7f
unknown <joshetito29@gmail.com>
2024-12-30 16:06:15 -0300

    maquetado adaptado casi

--- frontend/src/components/seat.component.js
+++ frontend/src/components/seat.component.js
@@ -1,8 +1,47 @@
+import React from "react";
+
 const seat = ({id, status}) => {
     return (
-        <div className="seat">
-            {id}
-        </div>
+        <><React.Fragment key={id}>
+            <div className="col-md-6">
+                {id.includes("A") && (
+                    <>
+                        <div className="row">
+                            <div
+                                className="col-md-6"
+                                key={id}
+                            >
+                                <div className="seat">
+                                    {id}
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
+
+            </div>
+        </React.Fragment>
+        <div />
+        <React.Fragment key={id}>
+                <div className="col-md-6">
+                    {id.includes("B") && (
+                        <>
+                            <div className="row">
+                                <div
+                                    className="col-md-6"
+                                    key={id}
+                                >
+                                    <div className="seat">
+                                        {id}
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                </div>
+            </React.Fragment>
+            </>
     );
 };
 
