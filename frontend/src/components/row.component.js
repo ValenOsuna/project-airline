28a20137599686be72e72b74a7f84da4f1e3489c
Valentino Osuna <Osunavalentino@gmail.com>
2025-01-02 21:34:01 -0300

    UPDATE: seats schema

--- frontend/src/components/row.component.js
+++ frontend/src/components/row.component.js
@@ -2,54 +2,22 @@ import React from "react";
 import Seat from "./seat.component";
 
 
-const Row = ({seatList, airplaneFare}) => {
-    console.log(airplaneFare)
+
+const Row = ({seatList, airplaneFare}) => { 
     return (
-    <div className="container col">  
-        {airplaneFare === "FC" &&(
-        <div className="row col-md-6">
+    <div className="col">  
+        <div className="row">
+            
             {seatList.map((seat) =>(
                 <Seat
                     key={seat.seat}
                     id={seat.seat}
                     status={seat.occupied}
-                    />
+                    /> 
+               
             )) }
+
         </div>
-        )}
-        {airplaneFare === "BC" &&(
-        <div className="row col-md-12">
-            {seatList.map((seat) =>(
-                <Seat
-                    key={seat.seat}
-                    id={seat.seat}
-                    status={seat.occupied}
-                    />
-            )) }
-        </div>
-        )}
-        {airplaneFare === "PC" &&(
-        <div className="row col-md-12">
-            {seatList.map((seat) =>(
-                <Seat
-                    key={seat.seat}
-                    id={seat.seat}
-                    status={seat.occupied}
-                    />
-            )) }
-        </div>
-        )}
-        {airplaneFare === "EC" &&(
-        <div className="row col-md-12">
-            {seatList.map((seat) =>(
-                <Seat
-                    key={seat.seat}
-                    id={seat.seat}
-                    status={seat.occupied}
-                    />
-            )) }
-        </div>
-        )}
     </div>
     );
 };
